256aab167eb0a49a8fcdfe549794f5bbf9355ad4
caroso1222 <ce.roso398@gmail.com>
2015-06-18 16:51:10 -0500

    Added 50 mails more

--- main/views.py
+++ main/views.py
@@ -25,7 +25,7 @@ from email.MIMEImage import MIMEImage
 
 
 ENVIAR_A_FOUNDERS = "NO"
-NUMERO_DE_PROPUESTAS = 120
+NUMERO_DE_PROPUESTAS = 170
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 csv_spas = BASE_DIR + "/csv-files/Clientes-SPA.csv"
